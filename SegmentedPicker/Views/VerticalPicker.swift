5b246b2b0d543ed0e6985d63a4f5b05f781d8aa3
ESmith <dev@scriffles.com>
2023-07-25 14:40:47 -0500

    add vertical picker

---
+++ SegmentedPicker/Views/VerticalPicker.swift
@@ -0,0 +1,55 @@
+//
+//  VerticalPicker.swift
+//  SegmentedPicker
+//
+//  Created by Scriffles on 7/25/23.
+//
+
+import SwiftUI
+
+struct VerticalPicker: View {
+    @Namespace private var selectionAnimation
+    @State private var selectedCity = "Seattle"
+    private let cities = ["Seattle","Spokane","Tacoma","Olympia"]
+    private let height:CGFloat = 40
+    private let width:CGFloat = 200
+    private let appColor:Color = .green
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            ForEach(cities, id: \.self) { city in
+                Button {
+                    withAnimation(.linear) {
+                        selectedCity = city
+                    }
+                } label: {
+                    Text(city)
+                        .foregroundColor(selectedCity == city ? .white : .gray.opacity(0.8))
+                        .font(.body)
+                        .frame(width: width, height: height)
+                        .background {
+                            if selectedCity == city {
+                                Capsule()
+                                    .foregroundColor(appColor)
+                                    .matchedGeometryEffect(id: "capsule", in: selectionAnimation)
+                            } else {
+                                Capsule()
+                                    .foregroundColor(appColor.opacity(0.01))
+                            }
+                        }
+                }
+            }
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(appColor.opacity(0.2))
+        }
+        .frame(width: width)
+    }
+}
+
+struct VerticalPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        VerticalPicker()
+    }
+}
