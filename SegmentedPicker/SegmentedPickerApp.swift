9b1d3bbec6ddb7d32efcae868e3ec98484db7991
ESmith <dev@scriffles.com>
2023-07-25 13:29:15 -0500

    Initial Commit

---
+++ SegmentedPicker/SegmentedPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedPickerApp.swift
+//  SegmentedPicker
+//
+//  Created by Scriffles on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
